9f1b3bc978eec5200f2347f6e36e83d95dfc9fce
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-09-13 18:37:48 -0400

    Update StdintTest.cpp

--- StdintTest.cpp
+++ StdintTest.cpp
@@ -4,9 +4,9 @@
 int main(){
 	char* oi;
 	char message[32] = {65, 115, 104, 101, 114};
-	oi = &message[0];
+	oi = &message[0]; //oi is accessing the first bytes address of message
 	for (int i = 0; i < sizeof(oi); i++) {
-		std::cout << oi[i];
+		std::cout << oi[i]; //post decrementing oi to write out every byte ascii character to the terminal until hotting the null byte, then exiting
 	};
 };
 
